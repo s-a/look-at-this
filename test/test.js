dfb646a944157b7bc7e796d7667c3127f553191f
s-a <stephan.ahlf@googlemail.com>
2015-03-20 10:35:12 +0100

    added test.js

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+var cfg = require("./ico/.scream.js");
+require('should');
+
+describe('valid build file config', function(){
+
+    it('it should contain color', function(){
+		for (var fn in cfg.images) {
+			var img = cfg.images[fn];
+			img.color.length.should.be.above(1);
+		}
+  	});
+});

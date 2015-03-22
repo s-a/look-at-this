04683ac5aad72151bc949d5c09a928d20d8ed7e4
s-a <stephan.ahlf@googlemail.com>
2015-03-22 22:39:34 +0100

    Function 'tmpl' has the same name as a variable

--- social-icon.js
+++ social-icon.js
@@ -3,7 +3,7 @@
 (function(){
 	var cache = {};
 	var i;
-	var tmpl = function tmpl(str, data){
+	var tmpl = function(str, data){
 	  // Figure out if we're getting a template, or if we need to
 	  // load the template - and be sure to cache the result.
 	  var fn = !/\W/.test(str) ?

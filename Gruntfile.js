cfc195fb93aeb8f0ec50da2ca4a716ca83733891
s-a <stephan.ahlf@googlemail.com>
2015-03-24 10:07:00 +0100

    Release v0.1.13

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
 				tagName: 'v%VERSION%',
 				tagMessage: 'Version %VERSION%',
 				push: true,
-				//pushTo: '',
+				pushTo: '',
 				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
 			}
 		},
